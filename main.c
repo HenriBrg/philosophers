53b70c21129f95b587030e78fb44e6be7c29baca
HenriBrg <hnr.berger@gmail.com>
2020-03-23 00:26:51 +0100

    Start

---
+++ main.c
@@ -0,0 +1,12 @@
+/* Ressources
+** Processus et thread : https://openclassrooms.com/fr/courses/1513891-la-programmation-systeme-en-c-sous-unix/1514339-les-processus
+** Langage C #17 (les threads) : https://www.youtube.com/watch?v=o_GbRujGCnM
+*/
+
+/*
+**	> Ne pas utiliser printf car la fonction affiche que lorsque son buffer est
+**	rempli, et non lors du call de la fonction
+**	> Utiliser les couleurs pour différencier les pthread
+*/
+
+// TODO : ficher + test all fonctions permises
